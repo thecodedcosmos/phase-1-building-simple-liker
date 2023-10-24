18c4eb9e1fd4bd41c6c3041765e7f51b73f89547
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-10-23 20:44:45 -0700

    added some JS, and added a class to the hidden modal

--- main.js
+++ main.js
@@ -3,8 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const articleHearts = document.querySelectorAll(".like-glyph");
 
+function likeCallback (e) {
+  const heart = e.target;
+  mimicServerCall()
+  .then (function(serverMessage) {
+    alert ("You notified the server!");
+    alert(serverMessage);
+    heart.innerText = glyphStates [heart.innerText];
+    heart.style.color = colorStates[heart.style.color];
+  })
 
+  .catch (function(error) {
+    alert ("Something went wrong!");
+  });
+
+}
+
+for (const glyoh of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
